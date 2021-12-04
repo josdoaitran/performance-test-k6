cc41eee08eae6344bf0d32f75c1905eba4ec4d8a
Doai Tran <josdoaitran@gmail.com>
2021-12-04 23:54:47 +0700

    Create example.js

---
+++ example.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export const options = {
+  // hosts: { 'test.k6.io': '1.2.3.4' },
+  stages: [
+    { duration: '1m', target: 10 },
+    { duration: '1m', target: 20 },
+    { duration: '1m', target: 0 },
+  ],
+  thresholds: { http_req_duration: ['avg<100', 'p(95)<200'] },
+  noConnectionReuse: true,
+  userAgent: 'MyK6UserAgentString/1.0',
+};
+
+export default function () {
+  http.get('http://test.k6.io/');
+}
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
